be4495f502accbdd6cbfc0cacf614e41e283797c
SonCuong1 <soncuong0307@gmail.com>
2024-01-31 17:17:06 +0700

    practice2

---
+++ exercise13/practice2/script.js
@@ -0,0 +1,42 @@
+// Bài 1:
+// let array = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// for (let i in array) {
+//     console.log("Row " + i);
+//     for (let j in array[i]) {
+//         console.log(array[i][j]);
+//     }
+// }
+
+// Bài 2:
+// let array = ['c', 's', 'c', '2', '6', '1'];
+// array.reverse();
+// array.forEach(function (word) {
+//     document.write(word);
+// })
+
+// Bài 3:
+// let array = [1, 3, 2, 8, 11, 0];
+// console.log(array.length);
+
+// Bài 4:
+// let string = "This is a string";
+// let count = 0;
+// console.log(string.length)
+
+// Bài 5:
+// let arrayA = ["This is a string"];
+// let arrayB = ["This is another string"];
+// if (arrayA === arrayB) {
+//     console.log("Both is the same");
+// } else {
+//     console.log("Both is not the same");
+// }
+
+// Bài 6:
+// let array = ["-", "a", "c", "d", "e", "word", 3, "-", "-", -2];
+// for (let i = 0; i < array.length - 1; i++) {
+//     if (array[i] === "-") {
+//         array.splice(i, 1, "_")
+//     }
+// }
+// console.log(array)
